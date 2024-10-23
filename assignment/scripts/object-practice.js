bfc28e25d1269cda325cc1ef7bce77ea846cb1e0
Blair Jilek <blairjilek@gmail.com>
2024-10-23 16:30:18 -0500

    added new property to shoes count and also to the favorite color

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -46,6 +46,10 @@ console.log(me.favThreeFoods[0]);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log(me.shoeCount);
+me.shoeCount = 7
+console.log(me.shoeCount);
+
 
 
 /* 5. Add a new property to an existing object.
@@ -54,3 +58,5 @@ console.log(me.favThreeFoods[0]);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = 'Green'
+console.log(me);
