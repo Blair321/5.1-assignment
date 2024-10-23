fab978f3a97a5e17d10157870bf3ddb39bc9fcbd
Blair Jilek <blairjilek@gmail.com>
2024-10-23 16:21:52 -0500

    added space for my fullname variable

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -26,7 +26,7 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-let fullName = (me.firstName) + (me.lastName);
+let fullName = (me.firstName) + " " + (me.lastName);
 console.log(fullName);
 
 
@@ -35,6 +35,8 @@ console.log(fullName);
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
+console.log(me.favThreeFoods[1]);
+console.log(me.favThreeFoods[0]);
 
 
 
